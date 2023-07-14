3c6f46f14622b5f962ef5df44a9f0e61dcd01854
Edi Carlos <edi_csilva@hotmail.com>
2023-07-14 16:22:43 -0300

    add font file

--- src/components/styles/Global.tsx
+++ src/components/styles/Global.tsx
@@ -1,20 +1,29 @@
 import { createGlobalStyle } from "styled-components";
+import Nunito from '../../fonts/nunito.ttf';
 
 export const theme = {
-    colors: {
-      header: '#eee',
-      body: "#fff",
-    },
-    mobile: '520px',
-    tablet: "768px",
-    laptop: "1024px"
-  }
+  colors: {
+    header: '#eee',
+    body: "#fff",
+  },
+  mobile: '520px',
+  tablet: "768px",
+  laptop: "1024px"
+}
 
-  export type MyProps = {
-    theme: typeof theme
-  }
+export type MyProps = {
+  theme: typeof theme
+}
 
 const GlobalStyles = createGlobalStyle<MyProps>`
+
+@font-face {
+  font-family: 'Nunito';
+  src: local('Nunito'),
+      url(${Nunito}) format('truetype');
+  font-weight: 400 700;
+  font-style: normal;
+}
 
 * {
     box-sizing: border-box;
@@ -25,7 +34,6 @@ body {
     color: #1f1f1f;
     font-family: 'Nunito', sans-serif;
     margin: 0;
-    font: 400 1rem Nunito, sans-serif;
 }
 
 p {
